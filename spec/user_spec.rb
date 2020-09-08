5daea4eb417b761e339fab4b2fcd12fa8f1f0f43
Alexis Simonian <alexis792.as@gmail.com>
2020-09-08 11:18:55 +0200

    modified tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,11 +3,23 @@
 require 'user'
 
 describe User do
+  let(:user){ User.create }
+
   it 'saves users into db' do
-    user = User.create
+    user
     User.create
     users = User.all
     expect(users.length).to eq(2)
     expect(users.first.id).to eq(user.id)
+  end
+
+  it 'can credit account' do
+    user.credit(100)
+    expect(user.balance).to eq(100)
+  end
+
+  it 'can debit account' do
+    user.debit(50)
+    expect(user.balance).to eq(-50)
   end
 end
